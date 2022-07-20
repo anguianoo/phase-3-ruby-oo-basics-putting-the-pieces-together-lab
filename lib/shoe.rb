db87ab1b14bc7bb76a380749943d592db49ce221
anguianoo <njanguiano@gmail.com>
2022-07-19 19:29:41 -0700

    added shoe and book class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+ attr_reader :brand
+ attr_accessor :color, :size, :material, :condition
+
+ def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+ end
+
+ def initialize(brand)
+    @brand = brand
+ end
+end
